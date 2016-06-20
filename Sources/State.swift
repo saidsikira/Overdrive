41d034e096541e9eeb5a3daea43efe06d4611bd7
SaidSikira <saidsikira@gmail.com>
2016-06-20 16:29:55 +0200

    Added documentation for State enum

--- Sources/State.swift
+++ Sources/State.swift
@@ -6,13 +6,35 @@
 //  Copyright © 2016 Said Sikira. All rights reserved.
 //
 
+/**
+ Internal task state
+*/
 enum State: Int, Comparable {
+    
+    /// Task state is `Initialized`
     case Initialized
+    
+    /// Task state is `Pending` and ready to evaluate conditions
     case Pending
+    
+    /// Task is ready to execute
     case Ready
+    
+    /// Task is executing
     case Executing
+    
+    /// Task is finished
     case Finished
     
+    /**
+     Check if current state can be changed to other state. 
+     You need to perform this check because task state can only occur 
+     in already defined way.
+     
+     - Parameter state: Target state
+     
+     - Returns: Boolean value indicating whether state change is possible
+    */
     func canTransitionToState(state: State) -> Bool {
         switch (self, state) {
         case (.Initialized, .Pending):
@@ -31,7 +53,12 @@ enum State: Int, Comparable {
     }
 }
 
+//MARK: - Comparable implementation
 
 func < (lhs: State, rhs: State) -> Bool {
     return lhs.rawValue < rhs.rawValue
+}
+
+func > (lhs: State, rhs: State) -> Bool {
+    return lhs.rawValue > rhs.rawValue
 }
