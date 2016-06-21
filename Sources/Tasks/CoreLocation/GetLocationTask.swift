a7dc3014903db48a61db2c05a06ffc3ff3ceecab
SaidSikira <saidsikira@gmail.com>
2016-06-21 13:12:06 +0200

    Added Tasks folder
    Tasks folder will be used to store most common iOS operation as tasks

--- Sources/Tasks/CoreLocation/GetLocationTask.swift
+++ Sources/Tasks/CoreLocation/GetLocationTask.swift
@@ -8,8 +8,27 @@
 
 import CoreLocation
 
-public class GetLocation: Task<CLLocation> {
+public class GetLocationTask: Task<CLLocation>, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    
     public override func run() {
-        
+        Dispatch.async(dispatch_get_main_queue()) {
+            self.manager.delegate = self
+            self.manager.desiredAccuracy = kCLLocationAccuracyBest
+            self.manager.startUpdatingLocation()
+        }
+    }
+    
+    @objc public func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            finish(.Value(location))
+
+        }
+    }
+    
+    public func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        finish(.Error(error))
     }
 }
