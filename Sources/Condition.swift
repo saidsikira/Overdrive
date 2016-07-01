c4aca8be9cd34f72ce7c04e5ae052b5508d3cb06
SaidSikira <saidsikira@gmail.com>
2016-07-01 10:55:42 +0200

    TaskConditionResult and TaskCondition documentation

--- Sources/Condition.swift
+++ Sources/Condition.swift
@@ -6,10 +6,19 @@
 //  Copyright © 2016 Said Sikira. All rights reserved.
 //
 
+/**
+ Defines task condition result that is returned in the process of evaluating
+ conditions. Can be `Satisfied` or `Failed(ErrorType)`
+*/
 public enum TaskConditionResult {
+    
+    /// Task conditions is satisfied
     case Satisfied
+    
+    /// Task condition failed with error
     case Failed(ErrorType)
     
+    /// If result is failed, associated error will be returned
     var error: ErrorType? {
         if case .Failed(let error) = self {
             return error
@@ -18,8 +27,31 @@ public enum TaskConditionResult {
     }
 }
 
+/**
+ Defines protocol that can be used to define conditions that should be satisfied in order
+ to run a task. Task conditions manage custom task dependencies and evaluation for the task.
+*/
 public protocol TaskCondition {
+    
+    /**
+     If task needs a dependency to execute first, you should return it in this method. For example,
+     some tasks need OS premissions to execute. For example location services, photo retrieval,
+     camera use etc. By using this method you can setup a dependency that can validate status of 
+     those premissions.
+     
+     - Parameter forTask: That that conditions are being evaluated for
+     
+     - Returns: `Task<U>` Any task instance
+    */
     func dependency<T, U>(forTask task: Task<T>) -> Task<U>?
+    
+    /**
+     Evaluates condition for the task. Evaluation can be any asynchronous process. When evaluation
+     process is done evaluationBlock callback should be called with appropriate `TaskConditionResult`.
+     
+     - Note: This method does not guarantee that evaluation will be done on the main thread. If you want
+     to evaluate condition on the main thread, use `dispatch_async` call.
+    */
     func evaluate<T>(forTask task: Task<T>, evaluationBlock: (TaskConditionResult -> Void))
 }
 
