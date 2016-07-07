6eda3bfddfbca96abd6cf68978984b34fb23f6be
SaidSikira <saidsikira@gmail.com>
2016-07-07 19:20:24 +0200

    Make changes on how tasks are added to the TaskQueue

--- Sources/TaskQueue.swift
+++ Sources/TaskQueue.swift
@@ -79,8 +79,8 @@ import class Foundation.NSOperationQueue
  
  `TaskQueue` has a custom delegate which can be used to monitor certain events
  in `TaskQueue` lifecycle. See `TaskQueueDelegate` for more information
-
-*/
+ 
+ */
 public class TaskQueue: NSOperationQueue {
     
     /**
@@ -92,7 +92,7 @@ public class TaskQueue: NSOperationQueue {
      let task = SomeTask()
      TaskQueue.main.addTask(task)
      ```
-    */
+     */
     public static var main: TaskQueue = {
         let queue = TaskQueue()
         queue.underlyingQueue = dispatch_get_main_queue()
@@ -108,7 +108,7 @@ public class TaskQueue: NSOperationQueue {
      let task = SomeTask()
      TaskQueue.background.addTask(task)
      ```
-    */
+     */
     public static var background: TaskQueue = {
         let queue = TaskQueue()
         queue.name = "BackgroundTaskQueue"
@@ -161,10 +161,22 @@ public class TaskQueue: NSOperationQueue {
             task.addObserver(retryObserver)
         }
         
-        addOperation(task)
+        _ = task
+            .conditions
+            .flatMap { $0.dependency(forTask: task) }
+            .map { task.addDependency($0); addOperation($0) }
+        
         delegate?.didAdd(task: task, toQueue: self)
         
-        task.willEnqueue()
+        addOperation(task)
+    }
+    
+    public override func addOperation(operation: NSOperation) {
+        super.addOperation(operation)
+        
+        if operation.respondsToSelector(#selector(Task<Any>.willEnqueue)) {
+            operation.performSelector(#selector(Task<Any>.willEnqueue))
+        }
     }
     
     /**
@@ -176,13 +188,13 @@ public class TaskQueue: NSOperationQueue {
     public func addTasks<T>(tasks: [Task<T>]) {
         _ = tasks.map { addTask($0) }
     }
-    
+
     /**
      Retries task execution. Method will decrease task retry count, set task
      state to `Initialized` and add it to the queue again.
      
      - Parameter task: Task to be retried
-    */
+     */
     func retry<T>(task task: Task<T>) {
         do {
             try task.decreaseRetryCount()
