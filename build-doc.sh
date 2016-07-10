41cfe0226ce7d90f504642300080c4213dec6b55
SaidSikira <saidsikira@gmail.com>
2016-07-10 22:35:08 +0200

    [doc] Update

--- build-doc.sh
+++ build-doc.sh
@@ -6,19 +6,31 @@ set -o pipefail
 PROJECT_NAME="Overdrive"
 DOC_FOLDER="latest"
 SOURCE_BRANCH="master"
-GITHUB_URL="https://github.com/swiftable/Overdrive"
+GITHUB_URL="https://github.com/arikis/Overdrive"
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $SOURCE_BRANCH ]; then exit 0; fi
 
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $SOURCE_BRANCH ]; then exit 0; fi
-
+echo "[GIT]   Cloning from $GITHUB_URL"
 if [ -d $PROJECT_NAME ]; then
-	git -C $PROJECT_NAME pull origin master
+	git -C $PROJECT_NAME pull origin $SOURCE_BRANCH &>build.log
 else
-	git clone --branch $SOURCE_BRANCH $GITHUB_URL
+	git clone --branch $SOURCE_BRANCH --single-branch $GITHUB_URL".git" &>build.log
 fi
 
-# Generate documentation with jazzy
+# Go to the Project folder
+cd $PROJECT_NAME 
 
-cd Overdrive &&
+# Fetch all tags
+git fetch --tags 
+
+# Get latest tag
+LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+# Checkout to the latest tag
+echo "[GIT]   Checking out $LATEST_TAG"
+git checkout $LATEST_TAG &>build.log
+
+echo "[JAZZY] Generating documentation . . ."
+# Generate documentation with jazzy
 jazzy \
 		--clean \
 		--swift-version 2.2 \
@@ -28,4 +40,6 @@ jazzy \
 		--theme fullwidth \
 		--head "$(cat ../head.html)" \
 		--github_url $GITHUB_URL \
+		--module Overdrive \
+		--module-version $LATEST_TAG \
 		--readme "README.md"
