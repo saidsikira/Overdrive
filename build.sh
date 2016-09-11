0e9d97cff2f3f4618cddd3dc817c14c83423d231
Said Sikira <saidsikira@gmail.com>
2016-09-11 22:39:48 +0200

    [travis] Update build.sh script and .travis.yml to enable testing on Xcode 8

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ SCHEME="Overdrive"
 IOS_SDK="iphonesimulator10.0"
 OSX_SDK="macosx10.11"
 TVOS_SDK="appletvsimulator10.0"
+WATCHOS_SDK="watchsimulator3.0"
 
 IOS_DESTINATION="OS=10.0,name=iPhone 6S"
 MACOS_DESTINATION="arch=x86_64"
@@ -19,81 +20,137 @@ usage() {
 cat << EOF
 Usage: sh $0 command
 
-	--docs		Build documentation using jazzy
-	--clean		Clean up all un-neccesary files
-	--all		Builds all iOS/macOS/tvOS/watchOS targets
-	--test		Runs full tests
-	--iOS		Build iOS target
-	--macOS		Build macOS target
-	--tvOS		Build tvOS target
-	--test-iOS	Test iOS target
-	--test-macOS	Test macOS target
-	--test-tvOS	Test tvOS target
+  [Building]
+
+  iOS           Build iOS framework
+  watchOS       Build watchOS framework
+  macOS         Build macOS framework
+  tvOS          Build tvOS framework
+  build         Builds all iOS/macOS/tvOS/watchOS targets
+  clean         Clean up all un-neccesary files
+
+  [Testing]
+
+  test-iOS      Test iOS target
+  test-macOS    Test macOS target
+  test-tvOS     Test tvOS target
+  test          Runs full tests
+
+  [Docs]
+
+  --docs          Build documentation using jazzy
 EOF
 }
 
 COMMAND="$1"
-
+  
 case "$COMMAND" in
-	"--clean")
-		find . -type d -name build -exec rm -r "{}" +\;
-		exit 0;
-	;;
-
-	"--docs")
-		jazzy \
-		--clean \
-		--author "Swiftable" \
-		--author_url "swiftable.io" \
-		--theme fullwidth \
-		--swift-version 2.2 \
-		--github_url "https://github.com/swiftable/Overdrive" \
-		--readme "./README.md"
-		exit 0;
-	;;
-
-	"--iOS")
-		xcodebuild clean -project $PROJECT -scheme "${SCHEME}" -sdk "${IOS_SDK}" -destination "${IOS_DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=YES build | xcpretty -c
-		exit 0;
-	;;
-	
-	"--macOS")
-		xcodebuild clean -project $PROJECT -scheme "${SCHEME}" -sdk "${MACOS_SDK}" -destination "${MACOS_DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=YES build | xcpretty -c
-		exit 0;
-	;;
-
-	"--tvOS")
-		xcodebuild clean -project $PROJECT -scheme "${SCHEME}" -sdk "${TVOS_SDK}" -destination "${TVOS_DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=YES build | xcpretty -c
-		exit 0;
-	;;
-
-	"--all")
-		sh $0 --iOS
-		sh $0 --macOS
-		sh $0 --tvOS
-		exit 0;
-	;;
-
-	"--test-iOS")
-		xcodebuild clean -project $PROJECT -scheme "${IOS_SCHEME}" -sdk "${IOS_SDK}" -destination "${IOS_DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=YES test | xcpretty -c
-		exit 0;
-	;;
-
-    "--test-macOS")
-    xcodebuild clean -project $PROJECT -scheme "${MACOS_SCHEME}" -sdk "${MACOS_SDK}" -destination "${MACOS_DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=YES test | xcpretty -c
+  "--clean")
+    find . -type d -name build -exec rm -r "{}" +\;
     exit 0;
-    ;;
+  ;;
 
-    "--test-tvOS")
-    xcodebuild clean -project $PROJECT -scheme "${TVOS_SCHEME}" -sdk "${TVOS_SDK}" -destination "${TVOS_DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=YES test | xcpretty -c
+  "--docs")
+    jazzy \
+    --clean \
+    --author "Swiftable" \
+    --author_url "swiftable.io" \
+    --theme fullwidth \
+    --swift-version 2.2 \
+    --github_url "https://github.com/swiftable/Overdrive" \
+    --readme "./README.md"
     exit 0;
-    ;;
+  ;;
 
-    "--test")
-        sh $0 --test-iOS
-        sh $0 --test-macOS
-        sh $0 --test-tvOS
-        exit 0;
-    ;;
+  "iOS" | "ios")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -sdk "${IOS_SDK}" \
+    -destination "${IOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    build | xcpretty -c
+    exit 0;
+  ;;
+
+  "watchOS" | "watchos")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -destination "${IOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    build | xcpretty -c
+    exit 0;
+  ;;
+
+  "macOS" | "macos")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -sdk "${MACOS_SDK}" \
+    -destination "${MACOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    build | xcpretty -c
+    exit 0;
+  ;;
+
+  "tvOS" | "tvos")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -sdk "${TVOS_SDK}" \
+    -destination "${TVOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    build | xcpretty -c
+    exit 0;
+  ;;
+
+  "build")
+    sh $0 iOS
+    sh $0 macOS
+    sh $0 tvOS
+    exit 0;
+  ;;
+
+  "test-iOS" | "test-ios")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -sdk "${IOS_SDK}" \
+    -destination "${IOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    test | xcpretty -c
+    exit 0;
+  ;;
+
+  "--test-macOS" | "test-macos")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -sdk "${MACOS_SDK}" \
+    -destination "${MACOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    test | xcpretty -c
+    exit 0;
+  ;;
+
+  "--test-tvOS" | "test-tvos")
+    xcodebuild clean \
+    -project $PROJECT \
+    -scheme "${SCHEME}" \
+    -sdk "${TVOS_SDK}" \
+    -destination "${TVOS_DESTINATION}" \
+    -configuration Debug ONLY_ACTIVE_ARCH=YES \
+    test | xcpretty -c
+    exit 0;
+  ;;
+
+  "test")
+    sh $0 test-iOS
+    sh $0 test-macOS
+    sh $0 test-tvOS
+    exit 0;
+  ;;
 esac
+
 usage
