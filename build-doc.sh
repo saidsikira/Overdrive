4d3c1cd5abfd2b60771a6a3a30cc2ebfb6585ca5
Said Sikira <saidsikira@gmail.com>
2016-09-20 16:37:02 +0200

    [doc] Update documentation
    * Update .jazzy.yaml
    * Update build-doc.sh

--- build-doc.sh
+++ build-doc.sh
@@ -7,38 +7,41 @@ PROJECT_NAME="Overdrive"
 DOC_FOLDER="latest"
 SOURCE_BRANCH="master"
 GITHUB_URL="https://github.com/arikis/Overdrive"
+
 #if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $SOURCE_BRANCH ]; then exit 0; fi
 
 echo "[GIT]   Cloning from $GITHUB_URL"
 if [ -d $PROJECT_NAME ]; then
-	git -C $PROJECT_NAME pull origin $SOURCE_BRANCH &>build.log
+	git -C $PROJECT_NAME pull origin $SOURCE_BRANCH
 else
-	git clone --branch $SOURCE_BRANCH --single-branch $GITHUB_URL".git" &>build.log
+	git clone --branch $SOURCE_BRANCH --single-branch $GITHUB_URL".git"
 fi
 
 # Go to the Project folder
-cd $PROJECT_NAME 
+pushd $PROJECT_NAME
 
 # Fetch all tags
-git fetch --tags 
+git fetch --tags
 
 # Get latest tag
 LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
+popd
+
 # Checkout to the latest tag
 echo "[GIT]   Checking out $LATEST_TAG"
-# git checkout $LATEST_TAG &>build.log
 
 echo "[JAZZY] Generating documentation . . ."
+
 # Generate documentation with jazzy
 jazzy \
 		--clean \
-		--output ../latest \
+		--output ./latest \
 		--author "Swiftable" \
 		--author_url "swiftable.io" \
 		--theme fullwidth \
-		--head "$(cat ../head.html)" \
+		--head "$(cat head.html)" \
+		--readme "Overdrive/README.md" \
 		--github_url $GITHUB_URL \
 		--module Overdrive \
-		--module-version $LATEST_TAG \
-		--readme "README.md"
+		--module-version $LATEST_TAG
